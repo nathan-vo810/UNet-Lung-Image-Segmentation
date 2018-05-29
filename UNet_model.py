3584ee3ab6d28ebec224edfdda9a57dc4102d46f
NhatAnh <nhatanh81096@yahoo.com>
2018-05-29 21:01:34 +0700

    Update Keras functions

--- UNet_model.py
+++ UNet_model.py
@@ -1,5 +1,6 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dropout, merge
+from keras.layers.merge import concatenate
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from load_data import *
@@ -48,32 +49,36 @@ class UNet(object):
 
         up6 = UpSampling2D(size=(2, 2))(drop5)
         up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up6)
-        merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
+        # merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
+        merge6 = concatenate([drop4, up6], axis=-1)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
         up7 = UpSampling2D(size=(2, 2))(conv6)
         up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up7)
-        merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
+        # merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
+        merge7 = concatenate([conv3, up7], axis=-1)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
         up8 = UpSampling2D(size=(2, 2))(conv7)
         up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up8)
-        merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
+        # merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
+        merge8 = concatenate([conv2, up8], axis=-1)
         conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
         conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
         up9 = UpSampling2D(size=(2, 2))(conv8)
         up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up9)
-        merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
+        # merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
+        merge9 = concatenate([conv1, up9], axis=-1)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
         conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
 
         conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
 
-        model = Model(input=inputs, output=conv10)
+        model = Model(inputs=inputs, outputs=conv10)
 
         model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
